f3c25380c5b6d6a0a39fb9f50c3ef8d90fff0f7c
janPiki <janpiki273@proton.me>
2025-06-28 16:10:42 +0700

    Initial commit

---
+++ raytiles.c
@@ -0,0 +1,70 @@
+#include "raytiles.h"
+#include "cJSON.h"
+#include <raylib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *ReadFile(char *filename) {
+  FILE *file = fopen(filename, "rb");
+  if (!file) {
+    printf("Failed to open file or something");
+    return NULL;
+  }
+
+  fseek(file, 0, SEEK_END);
+  long lenght = ftell(file);
+  rewind(file);
+
+  char *data = (char *)malloc(lenght + 1);
+  if (!data) {
+    fclose(file);
+    return NULL;
+  }
+
+  fread(data, 1, lenght, file);
+  data[lenght] = '\0';
+  fclose(file);
+  return data;
+}
+
+Layer LoadLayer(cJSON *layer) {
+  int width = cJSON_GetObjectItem(layer, "width")->valueint;
+  int height = cJSON_GetObjectItem(layer, "height")->valueint;
+  Layer l = CreateTileLayer((Vector2i){width, height});
+
+  char *typestr = cJSON_GetObjectItem(layer, "type")->valuestring;
+  LayerType layerType;
+  if (strcmp(typestr, "tilelayer") == 0) {
+    layerType = TILE_LAYER;
+  } else if (strcmp(typestr, "objectgroup")) {
+    layerType = OBJECT_GROUP;
+  } else if (strcmp(typestr, "imagelayer")) {
+    layerType = IMAGE_LAYER;
+  } else if (strcmp(typestr, "group")) {
+    layerType = GROUP;
+  }
+
+  return l;
+}
+
+void UnloadLayer(Layer layer) {
+  // TODO: Finish this function & everything else I guess
+}
+
+TileMap LoadTileMap(char *filename) {
+  cJSON *data = cJSON_Parse(ReadFile(filename));
+
+  TileMap t = CreateTileMap();
+  t.size.x = cJSON_GetObjectItem(data, "width")->valueint;
+  t.size.y = cJSON_GetObjectItem(data, "height")->valueint;
+
+  cJSON *layers = cJSON_GetObjectItem(data, "layers");
+  int layerNum = cJSON_GetArraySize(layers);
+
+  for (int i = 0; i < layerNum; i++) {
+    t.layers[i] = LoadLayer(cJSON_GetArrayItem(layers, i));
+  }
+
+  return t;
+}
---
+++ raytiles.h
@@ -0,0 +1,111 @@
+#ifndef RAYTILE
+#define RAYTILE
+
+#include <raylib.h>
+#include <stdbool.h>
+
+// Structs and enums
+typedef enum { INT, FLOAT, BOOL, STRING } PropertyType;
+typedef enum { TILE_LAYER, OBJECT_GROUP, IMAGE_LAYER, GROUP } LayerType;
+
+typedef struct {
+  int x, y;
+} Vector2i;
+
+typedef struct {
+  char *key;
+  PropertyType type;
+  union {
+    int i;
+    float f;
+    bool b;
+    char *s;
+  } Value;
+} TileProperties;
+
+typedef struct {
+  int id;
+  TileProperties *properties;
+  int propertiesCount;
+} Tile;
+
+typedef struct {
+  Vector2i size;
+  int TileCount;
+  Texture2D image;
+  Tile *tiles;
+} TileSet;
+
+// Layer types and stuff
+struct Layer;
+
+typedef struct {
+  Vector2i size;
+  int **data; // Double pointer. Spooky!
+} TileLayerData;
+
+typedef struct {
+  int id;
+  char *name;
+  char *type; // Defined by user
+  Vector2i position;
+  Vector2i size;
+} Object;
+
+typedef struct {
+  Object *objects;
+  int objectCount;
+} ObjectLayer;
+
+typedef struct {
+  char *filename;
+  Vector2i position;
+} ImageLayer;
+
+typedef struct {
+  struct Layer *layers;
+  int layerCount;
+} Group;
+
+typedef struct Layer {
+  char *name;
+  LayerType type;
+  union {
+    TileLayerData data;
+    ObjectLayer objLayer;
+    ImageLayer imageLayer;
+    Group group;
+  } LayerData;
+} Layer;
+
+typedef struct {
+  Vector2i size;
+  Vector2i tileSize;
+  TileSet tileSet;
+  Layer *layers;
+} TileMap;
+
+// Function declaration
+// Loading & unloading
+TileMap LoadTileMap(char *filename);
+void UnloadTileMap(TileMap tileMap);
+TileSet LoadTileSet(char *filename);
+void UnloadTileSet(TileSet tileSet);
+
+// Drawing and stuff
+void DrawTileMap(TileMap tileMap); // Draws all the layers of a TileMap
+void DrawLayer(Layer layer);       // Draw one TileMap Layer
+void DrawTile(Layer layer, Vector2i position); // Draw a single Tile
+
+// Type convertion
+Vector2i WorldToGrid(Vector2 worldPos); // Convert Vector2 position to grid pos
+Vector2 GridToWorld(Vector2i gridPos);  // Convert grid position to Vector2
+
+// Creation stuff
+TileMap CreateTileMap();
+// Pass 0 as the id to remove the Tile
+Tile CreateTile(TileSet tileSet, int id, Vector2i position);
+Layer CreateTileLayer(Vector2i size);
+TileSet CreateTileSet(Texture2D image);
+
+#endif
